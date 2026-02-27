b61ad4398f7d32dbdc8b5d88973941c1e748ee77
Valentin B <valentin.bourcier@inria.fr>
2026-02-27 10:37:49 +0100

    Merge main into g-dsu to update branch

--- backend/interpreter.py
+++ backend/interpreter.py
@@ -1,6 +1,7 @@
+from typing import Generator, List
 from types import GeneratorType
 
-from antlr4.RuleContext import RuleContext
+from antlr4.ParserRuleContext import ParserRuleContext
 from antlr4.tree.Tree import ParseTreeVisitor
 
 from backend.parser import Parser
@@ -11,6 +12,7 @@ class Environment:
     Language designer can dynamically set the attributes they need to keep track of.
 
     Example:
+
     - self._environment.attribute = value
     """
 
@@ -23,19 +25,19 @@ class Environment:
 
 class Visit:
 
-    def __init__(self, tree: RuleContext):
+    def __init__(self, tree: ParserRuleContext):
         self._tree = tree
 
     @property
-    def tree(self):
+    def tree(self) -> ParserRuleContext:
         return self._tree
-
 
 class Interpreter(ParseTreeVisitor):
     """
     AST visitor to extend in order to define the interpretation of a program.
 
     The interpreter requires the language designers to use Python generators through:
+
     - The yield keyword when visiting a subtree: yield self.visit(tree), yield self.visitChildren(tree).
     - The yield keyword instead of standard return when defining a method's return value.
 
@@ -57,33 +59,30 @@ class Interpreter(ParseTreeVisitor):
 
         :param parser: an instance of Parser class
         """
-        self._environment = Environment()
         self._parser = parser
 
+        # Default initializations
+        self._environment = Environment()
+        self._tree = None
+        self._halt = False
         self._interpretation_stack = []
         self._interpretation_result = None
         self._interpretation_steps = []
 
-        self._halt = False
 
-    def visit(self, tree):
+    def visit(self, tree) -> Visit:
         """
         Mark a node to visit by the interpretation loop.
+
         :param tree: the AST to be visited
         """
         return Visit(tree)
 
-    def interpret(self, code: str):
-        tree = self._parser.parse(code)
-        self._interpretation_stack = [self.visit(tree)]
-        self._interpretation_result = None
-        self._interpretation()
-
-    def _interpretation(self):
+    def _interpretation(self) -> None:
         while self._interpretation_stack and not self._halt:
             self._interpretation_step()
 
-    def _interpretation_step(self):
+    def _interpretation_step(self) -> None:
         while self._interpretation_stack and not self._halt:
             try:
                 current = self._interpretation_stack[-1]
@@ -105,30 +104,60 @@ class Interpreter(ParseTreeVisitor):
     def _should_end_step(self, result):
         return self._interpretation_steps and self._interpretation_steps[-1] == result
 
-    def visitChildren(self, node):
+    def visitChildren(self, node: ParserRuleContext) -> Generator[ParserRuleContext, None, None]:
         """
         Recursively visit the children of an AST node.
 
         :param node: the parent node of which to visit the children
-        :return: a list containing the results of the visit of the children
+        :return: a sequence of generators
         """
+        results = []
         n = node.getChildCount()
         for i in range(n):
             c = node.getChild(i)
-            if isinstance(c, RuleContext):
-                yield self.visit(c)
+            if isinstance(c, ParserRuleContext):
+                result = yield self.visit(c)
+                results.append(result)
+        yield results
 
-    def halt(self):
+    def interpret(self, code: str) -> None:
+        # Set the interpretation environment
+        self._environment = Environment()
+        self.initialize()
+
+        # Set the code to interpret
+        self._tree = self._parser.parse(code)
+
+        # Initialize the state of the interpretation
+        self._halt = False
+        self._interpretation_stack = [self.visit(self._tree)]
+        self._interpretation_result = None
+
+        # Start the interpretation loop
+        self._interpretation()
+
+    def initialize(self) -> None:
+        raise Exception("Implement this method to initialize the interpretation.")
+
+    def halt(self) -> None:
         self._halt = True
 
-    def proceed(self):
+    def proceed(self) -> None:
         self._halt = False
         self._interpretation()
 
-    def step(self):
+    def step(self) -> None:
         self._halt = False
         self._interpretation_step()
 
     @property
-    def environment(self):
+    def environment(self) -> Environment:
         return self._environment
+
+    @property
+    def tree(self) -> ParserRuleContext:
+        return self._tree
+
+    @property
+    def parser(self) -> Parser:
+        return self._parser
